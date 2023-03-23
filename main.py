560740ff9141d6f877323d555d854de55018fbdf
fenozo <960gd7@gmail.com>
2023-03-23 12:04:19 +0300

    centraliser le fichier de configuration de port de sortie

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from maga import socketio, app
 from maga.config import Configuration
 config = Configuration()
 if __name__ == '__main__':
-    socketio.run(app=app,debug=True, allow_unsafe_werkzeug=True, host=f'{config.getIpServer()}', port=9000)
+    socketio.run(app=app,debug=True, allow_unsafe_werkzeug=True, host=f'{config.getIpServer()}', port=f'{config.getPort()}')
