69581dc94d194a00f726057ec0ced30f581117b0
fenozo <960gd7@gmail.com>
2023-03-23 10:50:24 +0300

    enregistré le route de web_user

--- maga/__init__.py
+++ maga/__init__.py
@@ -6,6 +6,7 @@ from maga.Repository import Repository
 from flask_session import Session
 from maga.module_api_blf import api_blf
 from maga.module_api_user import api_user
+from maga.module_web_user import web_user
 from maga.module_web_main import module_web_main
 import json
 import datetime
@@ -22,7 +23,7 @@ def create_app():
     # front.register_blueprint(assets)
     # assets.register_blueprint(css)
     # app.register_blueprint(front)
-
+    app.register_blueprint(web_user)
     app.register_blueprint(api_blf)
     app.register_blueprint(api_user)
     app.register_blueprint(module_web_main)
--- maga/module_web_user/__init__.py
+++ maga/module_web_user/__init__.py
@@ -7,10 +7,10 @@ from maga.UserRepository import UserRepository
 from maga.BlfRepository import BlfRepository
 
 
-module_web_user = Blueprint('module_web_user', __name__)
+web_user = Blueprint('web_user', __name__)
 objectSession = SessionService(session=session)
 
 
-@module_web_user('/user/list')
+@web_user('/user/list')
 def user_list():
     return render_template('user_list.html')
