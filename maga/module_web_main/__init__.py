7b469f65f49fb3247b246f9c932fd902dfa33cd0
fenozo <960gd7@gmail.com>
2023-03-23 10:44:30 +0300

    Page pour la géstions des utilisateurs

--- maga/module_web_main/__init__.py
+++ maga/module_web_main/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 import socket
 
 from maga.config import Configuration
--- maga/module_web_user/__init__.py
+++ maga/module_web_user/__init__.py
@@ -9,3 +9,8 @@ from maga.BlfRepository import BlfRepository
 
 module_web_user = Blueprint('module_web_user', __name__)
 objectSession = SessionService(session=session)
+
+
+@module_web_user('/user/list')
+def user_list():
+    return render_template('user_list.html')
